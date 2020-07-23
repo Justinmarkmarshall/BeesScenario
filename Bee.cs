35dbad54644f6dfcfa40a02a0e0a95fd730d8015
Justinmarkmarshall <46491038+Justinmarkmarshall@users.noreply.github.com>
2020-07-23 22:11:51 +0100

    Add files via upload

---
+++ Bee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bees.Core
+{
+    [Serializable]
+    public abstract class Bee
+    {
+        #region Properties
+
+        public int Id { get; set; } = 0;
+
+        public double db_Health { get; private set; } = 0F;
+
+        public bool bDead { get; set; } = false;
+
+        #endregion Properties
+
+        #region Constructors
+
+        public Bee(double Health = 100) { db_Health = Health; }
+
+        #endregion Constructors
+
+        #region Public Methods
+
+        public abstract void Inflict_Damage(int iDamage);
+        //{
+        //    //if (bDead == false)
+        //    //{
+        //    //    var oldHealth = this.db_Health;
+
+        //    //    var newHealth = oldHealth - Convert.ToDouble(iDamage);
+
+        //    //    if (newHealth < 70)
+        //    //    {
+        //    //        this.bDead = true;
+        //    //    }
+        //    //}           
+        //}
+
+        public void UpdateHealth(double dValue)
+        {
+            this.db_Health = dValue;
+        }
+
+        #endregion Public Methods
+
+    }
+}
